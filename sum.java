d78809ccf1f012c35821af2e696dcf88aa9aee0b
Pratiksha Gargate <pratikshagargate1998@gmail.com>
2023-05-23 12:54:11 +0530

    sum.java modified

--- sum.java
+++ sum.java
@@ -2,6 +2,9 @@ class sum {
     public static void main() {
  
         System.out.print("In sum.java file");
+        System.out.print("In sum.java file");
+        System.out.print("In sum.java file");
+        System.out.print("In sum.java file");
 
     }
 }
